bbc14a0e2565dfb060725b1c66ed18b329e92ec7
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2025-01-12 01:57:15 +0300

    added configuration for the bot

--- monitorNode/keep_alive.js
+++ monitorNode/keep_alive.js
@@ -1,1 +1,17 @@
- 
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+export function keepAlive() {
+  app.listen(port, () => {
+    console.log("Server is ready.");
+  });
+} 
